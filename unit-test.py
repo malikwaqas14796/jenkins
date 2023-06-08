56cc467602e61a197f0f5c04eeed397c7e2189a2
Muhammad Waqas <waqas.rafique@nayatel.com>
2023-06-08 09:31:17 +0500

    Test change

--- unit-test.py
+++ unit-test.py
@@ -3,7 +3,7 @@ def check_python_syntax(code):
         compile(code, filename='<string>', mode='exec')
         print("Syntax check passed.")
     except SyntaxError as e:
-        assert "Syntax check failed: {e}"
+        raise AssertionError(f"Syntax check failed: {e}")
 
 # Provide your Python code here
 python_code = '''
